0c59afcd06673966f16d0406a6b4c4c77153f447
Enryoo <francisco.ormeno1@gmail.com>
2022-11-30 23:18:26 -0300

    server express

--- serverExpress.js
+++ serverExpress.js
@@ -1,27 +1,87 @@
-const express = require("express");
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
-const app = express();
+app.use(cors())
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send('<h1 style="color: blue">Bienvenidos al servidor express</h1>');
-});
+let notes = [
+  {
+    title: "Escuadra",
+    price: 123.45,
+    thumbnail: "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+    id: 1
+  },
+  {
+    title: "Calculadora",
+    price: 234.56,
+    thumbnail: "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
+    id: 2
+  },
+  {
+    title: "Globo Terráqueo",
+    price: 345.67,
+    thumbnail: "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
+    id: 3
+  }
+ 
+]
 
-let visitas = 1;
-app.get("/visitas", (req, res) => {
-  res.send({ message: `Cantidad de visitas es ${visitas++}` });
-});
+const generateId = () => {
+  const notesIds = notes.map(n => n.id)
+  const maxId = notesIds.length ? Math.max(...notesIds) : 0
+  const newId = maxId + 1
+  return newId
+}
 
-app.get("/fyh", (req, res) => {
-    res.send({ fyh: moment().format('DD/MM/YYYY:hh:mm') }
-    );
-  });
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
 
-const PORT = 8080;
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
 
-const server = app.listen(PORT, () => {
-  console.log(`Listening app port ${server.address().port}`);
-});
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
 
-server.on("error", (error) => {
-  console.log("Error", error);
-});
+  if (note) {
+    return response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+
+  if (!note.content) {
+    return response.status(400).json({
+      error: 'required "content" field is missing'
+    })
+  }
+
+  const newNote = {
+    id: generateId(),
+    content: note.content,
+    date: new Date(),
+    import: note.important || false
+  }
+
+  notes = notes.concat(newNote)
+
+  response.json(note)
+})
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
